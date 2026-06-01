52d5ebd704e2ea7f766ca9f0d59644a77a7e6734
Bruno Senzio-Savino <bruno@example.com>
2026-06-01 03:11:21 -0600

    changed arcade+

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Portfolio Loaded");
+// =========================
+// PROFILE IMAGE ROTATION
+// =========================
+
+const profileImages = [
+    "assets/images/profile.jpg",
+    "assets/images/profile1.jpg",
+    "assets/images/profile2.jpg",
+    "assets/images/profile3.jpg",
+    "assets/images/profile4.jpg",
+    "assets/images/profile5.jpg"
+];
+
+let currentProfile = 0;
+
+const profilePhoto = document.getElementById("profile-photo");
+
+setInterval(() => {
+
+    currentProfile++;
+
+    if(currentProfile >= profileImages.length){
+        currentProfile = 0;
+    }
+
+    profilePhoto.src = profileImages[currentProfile];
+
+}, 3000); // cambia cada 3 segundos
