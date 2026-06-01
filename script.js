71d72206a836d2e62b01005f06f2027e589946f9
Bruno Senzio-Savino <bruno@example.com>
2026-06-01 07:22:55 -0600

    added skill cartridge

--- script.js
+++ script.js
@@ -26,3 +26,104 @@ setInterval(() => {
     profilePhoto.src = profileImages[currentProfile];
 
 }, 3000); // cambia cada 3 segundos
+
+
+const slides =
+document.querySelectorAll(".skill-slide");
+
+let current = 0;
+
+function updateCarousel(){
+
+    slides.forEach((slide,index)=>{
+
+        let offset =
+        index-current;
+
+        if(offset < -2)
+            offset += slides.length;
+
+        if(offset > 2)
+            offset -= slides.length;
+
+        if(offset === 0){
+
+            slide.style.transform =
+            `
+            translate(-50%,-50%)
+            translateX(0px)
+            scale(1)
+            rotateY(0deg)
+            `;
+
+            slide.style.zIndex = 10;
+            slide.style.opacity = 1;
+        }
+
+        else if(offset === -1){
+
+            slide.style.transform =
+            `
+            translate(-50%,-50%)
+            translateX(-300px)
+            rotateY(45deg)
+            scale(.8)
+            `;
+
+            slide.style.zIndex = 5;
+            slide.style.opacity = .8;
+        }
+
+        else if(offset === 1){
+
+            slide.style.transform =
+            `
+            translate(-50%,-50%)
+            translateX(300px)
+            rotateY(-45deg)
+            scale(.8)
+            `;
+
+            slide.style.zIndex = 5;
+            slide.style.opacity = .8;
+        }
+
+        else{
+
+            slide.style.transform =
+            `
+            translate(-50%,-50%)
+            translateX(${offset*450}px)
+            scale(.6)
+            `;
+
+            slide.style.zIndex = 1;
+            slide.style.opacity = .2;
+        }
+
+    });
+
+}
+
+document
+.getElementById("nextSkill")
+.addEventListener("click",()=>{
+
+    current =
+    (current+1)%slides.length;
+
+    updateCarousel();
+});
+
+document
+.getElementById("prevSkill")
+.addEventListener("click",()=>{
+
+    current =
+    (current-1+slides.length)
+    %slides.length;
+
+    updateCarousel();
+});
+
+updateCarousel();
